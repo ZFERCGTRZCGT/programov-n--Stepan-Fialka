daeeb700f4fef25ac47222277f0ff7f7a40ea1bc
fialkas <fialkas@spskladno.cz>
2026-09-24 08:43:52 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+jmeno = input("jmeno:")
+vek = int(input("vek:"))
+
+print()
+print("Ahoj,", jmeno + "!")
+
+vek2 = vek + 10
+print("Za 10 let ti bude" , vek2 , "let.")
---
+++ main2.py
@@ -0,0 +1,10 @@
+c1 = float(input("Zadej 1 číslo: "))
+c2 = float(input("Zadej 2 číslo: "))
+
+print()
+print("Součet:" , c1 + c2)
+
+print("Rozdíl:" , c1 - c2)
+
+print()
+print("Součin:" , c1 * c2)
